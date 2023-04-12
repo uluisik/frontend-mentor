790be4d32bc4fe34ca68b3183208646698b361aa
ahmet <ahmet55uluisik05@gmail.com>
2023-04-12 23:43:31 +0300

    created solutions

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,36 +1,22 @@
-import { Flex, Text, Image, Spacer, Link } from "@chakra-ui/react";
-import { AiFillLinkedin, AiOutlineTwitter } from "react-icons/ai";
-import { FaFacebook } from "react-icons/fa";
+import { Flex, Text, Image, Spacer, Link } from '@chakra-ui/react';
+import { AiFillLinkedin, AiOutlineTwitter } from 'react-icons/ai';
+import { FaFacebook } from 'react-icons/fa';
 
 function Aside() {
   return (
     <Flex alignItems="center" mt="200" mb="20">
       <Flex w="250px" h="80px">
-        <Image p="6" src="image/logo-desktop.svg" />
+        <Image p="6" src="https://www.frontendmentor.io/static/images/logo-desktop.svg" />
       </Flex>
 
       <Spacer />
       <Flex h="80px" alignItems="center" justifyContent="center">
-        <Text
-          ml="4"
-          as="em"
-          fontWeight="bold"
-          cursor="pointer"
-          fontSize="36"
-          color="blue.300"
-        >
+        <Text ml="4" as="em" fontWeight="bold" cursor="pointer" fontSize="36" color="blue.300">
           <Link href="https://twitter.com/login?lang=tr" target="_blank">
             <AiOutlineTwitter />
           </Link>
         </Text>
-        <Text
-          ml="8"
-          as="em"
-          fontWeight="bold"
-          cursor="pointer"
-          fontSize="36"
-          color="blue.300"
-        >
+        <Text ml="8" as="em" fontWeight="bold" cursor="pointer" fontSize="36" color="blue.300">
           <Link href="https://tr.linkedin.com/" target="_blank">
             <AiFillLinkedin />
           </Link>
--- src/pages/Solutions/Solutions.tsx
+++ src/pages/Solutions/Solutions.tsx
@@ -1,7 +1,120 @@
-import React from 'react';
+import { Flex, Heading, Image, Spacer, Tab, TabList, Tabs, Text } from '@chakra-ui/react';
+import Aside from '../../components/Aside';
+import MainSearch from '../../components/Main/MainSearch';
 
 function Solutions() {
-  return <div>Solutions</div>;
+  return (
+    <>
+      <Flex
+        border="1px solid"
+        borderColor="gray.300"
+        borderLeftColor="white"
+        borderRightColor="white"
+        h="60px"
+        mb="8"
+      >
+        <Flex
+          alignItems="center"
+          ml="8"
+          borderLeft="1px solid"
+          borderRight="1px solid"
+          borderColor="gray.300"
+          px="8"
+          fontWeight="bold"
+          fontSize="lg"
+        >
+          SOLUTIONS
+        </Flex>
+        <Spacer />
+        <Flex alignItems="center" mr="12" fontWeight="semibold" fontSize="md">
+          <Text _hover={{ borderBottom: '3px solid red' }} cursor="pointer" mr="8">
+            Solutions
+          </Text>
+          <Text _hover={{ borderBottom: '3px solid red' }} cursor="pointer" mr="8">
+            Tags
+          </Text>
+          <Text _hover={{ borderBottom: '3px solid red' }} cursor="pointer">
+            Wall of Fame
+          </Text>
+        </Flex>
+      </Flex>
+      <Tabs mb="6" colorScheme="blue" ml="20" w="25%">
+        <TabList>
+          <Tab fontSize="lg" fontWeight="bold">
+            Feedback requested
+          </Tab>
+          <Tab fontSize="lg" fontWeight="bold">
+            All solutions
+          </Tab>
+        </TabList>
+      </Tabs>
+      <Flex
+        border="1px solid"
+        borderColor="gray.200"
+        flexDirection="column"
+        w="416px"
+        h="580px"
+        borderRadius="10"
+        position="relative"
+      >
+        <Image
+          _hover={{ WebkitTransform: 'scale(1.1)' }}
+          cursor="pointer"
+          mb="6"
+          src="image/news.webp"
+          borderRadius="10"
+        />
+        <Text>Submitted about 1 hour ago</Text>
+        <Heading
+          cursor="pointer"
+          _hover={{ borderBottom: '3px solid' }}
+          fontSize="2xl"
+          w="-webkit-max-content"
+        >
+          React password generator
+        </Heading>
+        <Text>#react #vite #typescript</Text>
+        <Flex my="6">
+          <Text mr="2" fontWeight="bold" fontSize="xl">
+            HTML
+          </Text>
+          <Text mr="2" fontWeight="bold" fontSize="xl">
+            CSS
+          </Text>
+          <Spacer />
+          <Flex mr="2" borderRadius="lg">
+            <Text
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              w="20px"
+              color="white"
+              border="1px solid "
+              borderLeftRadius="lg"
+              fontWeight="bold"
+            >
+              AAA
+            </Text>
+            <Text
+              display="flex"
+              alignItems="center"
+              justifyContent="center"
+              fontWeight="bold"
+              border="1px solid"
+              borderRightRadius="lg"
+              px="2"
+            >
+              AAAAA
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+      <Flex flexDirection="column" alignItems="center" justifyContent="center">
+        <MainSearch />
+      </Flex>
+      <Aside />
+    </>
+  );
 }
 
 export default Solutions;
