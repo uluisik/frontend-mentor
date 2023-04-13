9caa5a705ada8f51624bb34fb34d13255e29c208
ahmet <ahmet55uluisik05@gmail.com>
2023-04-13 17:24:57 +0300

    improved tags pages

--- src/pages/Tags/Tags.tsx
+++ src/pages/Tags/Tags.tsx
@@ -1,40 +1,56 @@
-import { Flex, Spacer, Text } from '@chakra-ui/react';
+import { Flex, Input, Spacer, Text } from '@chakra-ui/react';
+import Aside from '../../components/Aside';
 
 function Tags() {
   return (
-    <Flex
-      border="1px solid"
-      borderColor="gray.300"
-      borderLeftColor="white"
-      borderRightColor="white"
-      h="60px"
-      mb="8"
-    >
+    <>
       <Flex
-        alignItems="center"
-        ml="8"
-        borderLeft="1px solid"
-        borderRight="1px solid"
+        border="1px solid"
         borderColor="gray.300"
-        px="8"
-        fontWeight="bold"
-        fontSize="lg"
+        borderLeftColor="white"
+        borderRightColor="white"
+        h="60px"
+        mb="8"
       >
-        SOLUTIONS
+        <Flex
+          alignItems="center"
+          ml="8"
+          borderLeft="1px solid"
+          borderRight="1px solid"
+          borderColor="gray.300"
+          px="8"
+          fontWeight="bold"
+          fontSize="lg"
+        >
+          SOLUTIONS
+        </Flex>
+        <Spacer />
+        <Flex alignItems="center" mr="12" fontWeight="semibold" fontSize="md">
+          <Text _hover={{ borderBottom: '3px solid red' }} cursor="pointer" mr="8">
+            Solutions
+          </Text>
+          <Text _hover={{ borderBottom: '3px solid red' }} cursor="pointer" mr="8">
+            Tags
+          </Text>
+          <Text _hover={{ borderBottom: '3px solid red' }} cursor="pointer">
+            Wall of Fame
+          </Text>
+        </Flex>
       </Flex>
-      <Spacer />
-      <Flex alignItems="center" mr="12" fontWeight="semibold" fontSize="md">
-        <Text _hover={{ borderBottom: '3px solid red' }} cursor="pointer" mr="8">
-          Solutions
-        </Text>
-        <Text _hover={{ borderBottom: '3px solid red' }} cursor="pointer" mr="8">
+      <Flex mt="20" mx="20">
+        <Text fontWeight="88px" fontSize="4xl">
           Tags
         </Text>
-        <Text _hover={{ borderBottom: '3px solid red' }} cursor="pointer">
-          Wall of Fame
-        </Text>
+        <Spacer />
+        <Input
+          h="50px"
+          w="300px"
+          placeholder="Search tags..."
+          _placeholder={{ fontSize: 'lg' }}
+        ></Input>
       </Flex>
-    </Flex>
+      <Aside />
+    </>
   );
 }
 
